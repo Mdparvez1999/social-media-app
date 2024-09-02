283a4375e420975d51bf7883037fa8ffa802b7be
Mdparvez19 <mohmdparvez78@gmail.com>
2024-09-02 12:32:48 +0530

    Remove dist folder from repository

--- server/dist/routes/chats.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { ChatsController } from "../controllers/chats.controllers.js";
-import { auth } from "../middlewares/auth.Middleware.js";
-const router = express.Router();
-const chatsController = new ChatsController();
-router.get("/conversations", auth, chatsController.getAllConversations);
-router.post("/send-message/:recieverId", auth, chatsController.sendMessage);
-router.get("/all-messages/:conversationId", auth, chatsController.getAllMessages);
-export default router;
