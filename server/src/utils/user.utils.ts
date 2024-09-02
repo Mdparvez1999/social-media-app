c261a3f15df8a35334bbe9daf70897eed84edb1d
Mdparvez19 <mohmdparvez78@gmail.com>
2024-09-02 22:42:51 +0530

    fixed auth controller bug

--- server/src/utils/user.utils.ts
+++ server/src/utils/user.utils.ts
@@ -24,12 +24,20 @@ export class UserUtils {
     return user;
   }
 
+  public static async checkEmailExists(email: string) {
+    if (!email) throw new AppError("user email is required", 400);
+
+    const userExists = await this.userRepository.findOneBy({ email });
+
+    if (userExists) throw new AppError("Email already exists", 400);
+  }
+
   public static async findUserByEmail(email: string): Promise<Users | null> {
     if (!email) throw new AppError("user email is required", 400);
 
     const user = await this.userRepository.findOneBy({ email });
 
-    if (user) throw new AppError("Email already exists", 400);
+    if (!user) throw new AppError(" user not found", 400);
 
     return user;
   }
